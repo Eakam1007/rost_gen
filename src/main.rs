eecd785f012520118b4674d0356f07bff3c3d754
Eakampreet <eakam1007@gmail.com>
2022-09-12 22:08:41 -0400

    Add support for vesion flags

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("WIP");
+        return;
+    }
+
+    let option_arg = &args[1];
+
+    if option_arg == "-v" || option_arg == "--version" {
+        println!("Version 0.1");
+    } else {
+        println!("Invalid option {}", option_arg);
+    }
 }
