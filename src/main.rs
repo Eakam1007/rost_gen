a3dbc24c849e2eb2f0f42bd96c9116d49b799e96
Eakampreet <eakam1007@gmail.com>
2022-09-14 23:34:38 -0400

    Add html template to output file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
+use std::io::{self, BufRead, Write};
 use std::{env, fs, path};
-use std::io::{self, BufRead};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -25,7 +25,7 @@ fn main() {
         }
 
         let input_path = &args[2];
-        let path =  path::Path::new(input_path);
+        let path = path::Path::new(input_path);
 
         if !path.exists() {
             println!("Invalid path: No file or directory found at {input_path}");
@@ -36,7 +36,7 @@ fn main() {
             fs::remove_dir_all("./dist").expect("Delete existing output directory")
         }
         fs::create_dir_all("./dist").expect("Create output directory");
-        
+
         if path.is_dir() {
             let dir = fs::read_dir(input_path);
             println!("WIP");
@@ -44,21 +44,33 @@ fn main() {
         }
 
         if path.is_file() {
-            if path.extension().unwrap().to_str().unwrap() != "txt" { 
+            if path.extension().unwrap().to_str().unwrap() != "txt" {
                 println!("Only .txt files are accepted");
                 return;
             }
-    
+
             let in_file = fs::File::open(input_path).expect(&format!("Open file at {input_path}"));
+            let html_template =
+                fs::read_to_string("./output_template.html").expect("Read template file");
             let mut buf_reader = io::BufReader::new(in_file);
             let html_file_name = path.file_stem().unwrap().to_str().unwrap();
             let mut read_buffer = String::new();
             let mut read_bytes: u64 = 0;
-    
+            let mut out_file = fs::OpenOptions::new()
+                .append(true)
+                .create_new(true)
+                .open(format!("./dist/{html_file_name}.html"))
+                .expect("Generate html file");
+
+            writeln!(out_file, "{}", html_template).expect("Generate html file");
+
             while read_bytes < fs::metadata(input_path).expect("Read input file").len() {
-                read_bytes += buf_reader.read_line(&mut read_buffer).expect("Read input file") as u64;
-                fs::write(format!("./dist/{html_file_name}.html"), read_buffer.clone()).expect("Generate html file");
+                read_bytes += buf_reader
+                    .read_line(&mut read_buffer)
+                    .expect("Read input file") as u64;
+                writeln!(out_file, "{}", read_buffer.clone()).expect("Generate html file");
             }
+            writeln!(out_file, "</body>\n</html>").expect("Generate html file");
         }
     } else {
         println!("Invalid option. Run rost_gen [-h | --help] for a list of options");
