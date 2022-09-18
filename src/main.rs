7fec5c707e5a47e0765d1ee35ff89577dd7b39be
Eakampreet <eakam1007@gmail.com>
2022-09-18 16:28:03 -0400

    Add option to specify an output directory

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::{env, fs, path};
 
 const HTML_TEMPLATE: &str = "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n\t<meta charset=\"UTF-8\">\n\t<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n\t<title>{{title}}</title>\n</head>\n<body>\n\t<p>\n";
 const VERSION: &str = "rost_gen version 0.1";
+const DEFAULT_OUTPUT_DIR: &str = "./dist";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -32,12 +33,17 @@ fn print_help_message() {
     println!("\t-h, --help\t\t\tPrint help message");
     println!("\t-i, --input [PATH]\n\t\tProvided a txt file path, will generate an html file");
     println!("\t\tProvided a directory path, will generate html files based on txt files in the directory");
-    println!("\t\tWARNING: Will output to ./dist directory and will delete all existing contents if it already exists");
+    println!("\t\tWARNING: By default, will output to ./dist directory and will delete all existing contents if it already exists");
+    println!("\n\t\tOptional: Use -o, --output [PATH] to output to a specific directory:");
+    println!("\t\t\t-i, --input [INPUT_PATH] -o, --output [OUTPUT_PATH]");
+    println!("\t\t This will not delete existing contents but will create the directory if it doesn't exist");
 }
 
 fn handle_conversion(args: &Vec<String>) {
-    if args.len() != 3 {
-        println!("Please provide a file or folder path. Enclose paths with spaces in single quotes");
+    // Handle no input path or
+    // input path with spaces which is not enclosed in quotes
+    if args.len() < 3 || (args.len() > 3 && (args[3] != "-o" && args[3] != "--output")) {
+        println!("Please provide a file or folder path. Enclose paths with spaces in quotes.");
         return;
     }
 
@@ -48,20 +54,18 @@ fn handle_conversion(args: &Vec<String>) {
         println!("Invalid path: No file or directory found at '{input_path}'");
         return;
     }
-
-    if path::Path::new("./dist").exists() {
-        fs::remove_dir_all("./dist").expect("Delete existing output directory")
-    }
-    fs::create_dir_all("./dist").expect("Create output directory");
+    
+    let mut output_dir_path = DEFAULT_OUTPUT_DIR.to_string();
+    create_output_directory(args, &mut output_dir_path);
 
     if path.is_dir() {
         let dir = fs::read_dir(input_path).expect("Read input directory");
-        convert_files_in_directory(dir);
+        convert_files_in_directory(dir, &output_dir_path);
     }
 
     if path.is_file() {
         if path.extension().unwrap().to_str().unwrap() == "txt" {
-            convert_file(input_path, path);
+            convert_file(input_path, path, &output_dir_path);
         } else {
             println!("Only .txt files are accepted");
             return;
@@ -69,7 +73,22 @@ fn handle_conversion(args: &Vec<String>) {
     }
 }
 
-fn convert_files_in_directory(dir: fs::ReadDir) {
+fn create_output_directory(args: &Vec<String>, output_dir_path: &mut String) {
+    if args.len() == 4 && (args[3] == "-o"|| args[3] == "--output") {
+        println!("WARNING: Output option specified but no output path specified. Defaulting to {DEFAULT_OUTPUT_DIR}");
+    } else if args.len() == 5 {
+        *output_dir_path = args[4].clone();
+    } else if args.len() > 5 {
+        println!("Invalid output path. Enclose output path in quotes if it contains spaces. Defaulting to {DEFAULT_OUTPUT_DIR}")
+    }
+
+    if output_dir_path == DEFAULT_OUTPUT_DIR && path::Path::new(DEFAULT_OUTPUT_DIR).exists() {
+        fs::remove_dir_all(DEFAULT_OUTPUT_DIR).expect("Delete existing output directory")
+    }
+    fs::create_dir_all(output_dir_path).expect("Create output directory");
+}
+
+fn convert_files_in_directory(dir: fs::ReadDir, output_dir_path: &String) {
     for entry in dir {
         let path_string = &entry
             .expect("Read directory files")
@@ -78,11 +97,11 @@ fn convert_files_in_directory(dir: fs::ReadDir) {
             .unwrap()
             .to_string();
         let path = path::Path::new(path_string);
-        convert_file(path_string, path);
+        convert_file(path_string, path, &output_dir_path);
     }
 }
 
-fn convert_file(path_string: &String, path: &path::Path) {
+fn convert_file(path_string: &String, path: &path::Path, output_dir_path: &String) {
     if path.extension().unwrap().to_str().unwrap() != "txt" {
         return;
     }
@@ -92,10 +111,11 @@ fn convert_file(path_string: &String, path: &path::Path) {
     let html_file_name = path.file_stem().unwrap().to_str().unwrap();
     let mut read_buffer = String::new();
     let mut read_bytes: u64 = 0;
+    let out_file_path = path::PathBuf::from(output_dir_path).join(format!("{html_file_name}.html"));
     let mut out_file = fs::OpenOptions::new()
         .append(true)
-        .create_new(true)
-        .open(format!("./dist/{html_file_name}.html"))
+        .create(true)
+        .open(out_file_path)
         .expect("Generate html file");
 
     write!(
