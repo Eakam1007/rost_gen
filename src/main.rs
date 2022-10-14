ddad3275b514eba82666866b2ce4f6e032a006fe
Eakampreet <eakam1007@gmail.com>
2022-10-14 09:34:33 -0400

    Refactor main.rs to improve readability and reduce duplication:
      * Spacing fixes
    
      * Combine two if statements into one check
    
      * Define new function to check if conversion file extension is valid
    
      * Use variable to store extension before checking if it is valid
    
      * Rename read bytes count variable
    
      * Use substring of read buffer instead of removing characters from it

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,10 @@ extern crate serde_with;
 use serde::{Deserialize, Serialize};
 #[derive(Debug, Deserialize, Serialize)]
 #[serde_with::skip_serializing_none]
-struct Config{
+struct Config {
     input: Option<String>,
     output: Option<String>,
-    lang: Option<String>
+    lang: Option<String>,
 }
 
 const HTML_TEMPLATE: &str = "<!DOCTYPE html>\n<html lang=\"{{lang}}\">\n<head>\n\t<meta charset=\"UTF-8\">\n\t<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n\t<title>\n\t\t{{title}}\n\t</title>\n</head>\n<body>\n";
@@ -21,9 +21,9 @@ const DEFAULT_OUTPUT_DIR: &str = "./dist";
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Take config written in JSON file as argument to parse into system 
+    /// Take config written in JSON file as argument to parse into system
     #[arg(short, long, value_name = "CONFIG_PATH")]
-    config: Option <String>,
+    config: Option<String>,
 
     /// Convert file/files in directory at INPUT_PATH into html files, outputting into ./dist directory by default (deleting existing contents)
     #[arg(short, long, value_name = "INPUT_PATH")]
@@ -41,9 +41,10 @@ struct Args {
 fn main() {
     // let args: Vec<String> = env::args().collect();
     let args = Args::parse();
-    if let Some (input) = args.input.as_deref() {
+
+    if let Some(input) = args.input.as_deref() {
         handle_conversion(input, &args.output, &args.lang);
-    } else if let Some (config) = args.config.as_deref() {
+    } else if let Some(config) = args.config.as_deref() {
         handle_config(config);
     }
 }
@@ -57,20 +58,17 @@ fn handle_config(config: &str) {
         return;
     }
 
-    if path.is_file() {
-        if path.extension().unwrap().to_str().unwrap() == "json" {
-            println!("reading json file at  '{config_path}'");
-            let file = File::open(config_path).unwrap();
-            let reader = BufReader::new(file);
+    if path.is_file() && path.extension().unwrap().to_str().unwrap() == "json" {
+        println!("reading json file at  '{config_path}'");
+        let file = File::open(config_path).unwrap();
+        let reader = BufReader::new(file);
 
-            let construct: Config = serde_json::from_reader(reader).unwrap();
-            let dept_input = construct.input.unwrap_or(" ".to_string());
-            let dept_output = construct.output.unwrap_or(DEFAULT_OUTPUT_DIR.to_string());
-            let dept_lang = construct.lang.unwrap_or("en-CA".to_string());
+        let construct: Config = serde_json::from_reader(reader).unwrap();
+        let dept_input = construct.input.unwrap_or(" ".to_string());
+        let dept_output = construct.output.unwrap_or(DEFAULT_OUTPUT_DIR.to_string());
+        let dept_lang = construct.lang.unwrap_or("en-CA".to_string());
 
-            handle_conversion(&dept_input, &dept_output, &dept_lang)
-
-        }
+        handle_conversion(&dept_input, &dept_output, &dept_lang)
     } else {
         println!("Only .json files are accepted");
         return;
@@ -94,15 +92,11 @@ fn handle_conversion(input: &str, output_dir_path: &String, html_lang: &String) 
         convert_files_in_directory(dir, output_dir_path, html_lang);
     }
 
-    if path.is_file() {
-        if path.extension().unwrap().to_str().unwrap() == "txt"
-            || path.extension().unwrap().to_str().unwrap() == "md"
-        {
-            convert_file(&input_path, path, output_dir_path, html_lang);
-        } else {
-            println!("Only .txt or .md files are accepted");
-            return;
-        }
+    if path.is_file() && conversion_file_path_valid(path) {
+        convert_file(&input_path, path, output_dir_path, html_lang);
+    } else {
+        println!("Only .txt or .md files are accepted");
+        return;
     }
 
     println!("Conversion successful. Output file(s) placed in directory at {output_dir_path}");
@@ -137,9 +131,7 @@ fn convert_file(
     html_lang: &String,
 ) {
     // We only want to convert .txt files
-    if path.extension().unwrap().to_str().unwrap() != "txt"
-        && path.extension().unwrap().to_str().unwrap() != "md"
-    {
+    if !conversion_file_path_valid(path) {
         return;
     }
 
@@ -153,7 +145,7 @@ fn convert_file(
     // Try to find title
     // Title is first line followed by two blank lines
     let mut title = String::new();
-    let mut read_bytes: u64 = parse_title_from_file(path_string, &mut title);
+    let mut read_bytes_count: u64 = parse_title_from_file(path_string, &mut title);
 
     // Variables to create and write to output html file
     let html_file_name = path.file_stem().unwrap().to_str().unwrap();
@@ -185,14 +177,14 @@ fn convert_file(
         write!(out_file, "\t<h1>\n\t\t{title}\t</h1>\n\t<p>\n").expect("Generate html file");
         // Skip the title bytes (first three lines) to prevent printing title twice
         buf_reader
-            .seek(io::SeekFrom::Start(read_bytes))
+            .seek(io::SeekFrom::Start(read_bytes_count))
             .expect("Read input file");
     }
 
     // Write the rest of the contents
-    while read_bytes < fs::metadata(path_string).expect("Read input file").len() {
+    while read_bytes_count < fs::metadata(path_string).expect("Read input file").len() {
         read_buffer.clear();
-        read_bytes += buf_reader
+        read_bytes_count += buf_reader
             .read_line(&mut read_buffer)
             .expect("Read input file") as u64;
         // Add paragraph tags if line is an empty line
@@ -210,9 +202,7 @@ fn convert_file(
             }
 
             if read_buffer.starts_with("# ") {
-                read_buffer.remove(0);
-                read_buffer.remove(0);
-                write!(out_file, "<h1>\n{read_buffer}</h1>\n").expect("Generate html file");
+                write!(out_file, "<h1>\n{}</h1>\n", &read_buffer[2..]).expect("Generate html file");
             } else if read_buffer == "---\n" || read_buffer == "---\r\n" {
                 write!(out_file, "<hr />\n").expect("Generate html file");
             } else {
@@ -253,4 +243,14 @@ fn parse_title_from_file(path_string: &String, title: &mut String) -> u64 {
     } else {
         return 0;
     }
+}
+
+fn conversion_file_path_valid(path: &path::Path) -> bool {
+    let extension = path.extension().unwrap().to_str().unwrap();
+
+    if extension == "txt" || extension == "md" {
+        return true;
+    }
+
+    return false;
 }
