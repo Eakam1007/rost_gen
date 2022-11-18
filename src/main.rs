eb8740e781bc5140c68e55cacecde775e934b7f6
Eakampreet <eakam1007@gmail.com>
2022-11-18 03:29:13 -0500

    intentionally break test

--- src/main.rs
+++ src/main.rs
@@ -479,7 +479,7 @@ mod tests {
     create_output_directory(&output_dir_path_string);
 
     assert!(output_dir_path.is_dir());
-    assert_eq!(
+    assert_ne!(
       output_dir_path.read_dir().unwrap().next().unwrap().expect("").path(),
       existing_file_path
     )
