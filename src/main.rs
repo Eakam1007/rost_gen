c3eb749a6e268fbc87ea1f40b023c1eb011c91e6
Wonkeun No <zakk23v@hotmail.com>
2022-11-18 22:21:24 -0500

    Replace old test with creates_html_in_directory

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use clap::Parser;
 use serde::{Deserialize, Serialize};
-use std::fs::{File};
+use std::fs::File;
 use std::io::{self, BufRead, BufReader, Seek, Write};
 use std::{fs, path};
-use stdio_override::StdoutOverride;
 #[derive(Debug, Deserialize, Serialize)]
 #[serde_with::skip_serializing_none]
 struct Config {
@@ -315,7 +314,7 @@ mod tests {
   use std::io::Read;
 
   use crate::*;
-  
+
   #[test]
   fn converts_txt_files() {
     let input_file_path = path::Path::new("sample.txt");
@@ -526,24 +525,49 @@ mod tests {
   }
 
   #[test]
-  fn handle_invalid_config() {
+  fn creates_html_in_directory() {
     let temp_dir = tempfile::tempdir().unwrap();
-    let test_input_path = temp_dir.path().join("test_config.txt");
-    let test_input_path_string = test_input_path.as_os_str().to_str().unwrap().to_string();
-    let mut test_input_file = File::create(&test_input_path).unwrap();
-    writeln!(test_input_file, "test").expect("Create test config file");
-    let expected_output = "Only .json files are accepted\n";
+    let test_input_path1 = temp_dir.path().join("html_template_test1.txt");
+    let test_input_path2 = temp_dir.path().join("html_template_test2.txt");
+    let mut test_input_file1 = File::create(&test_input_path1).unwrap();
+    let mut test_input_file2 = File::create(&test_input_path2).unwrap();
+    writeln!(test_input_file1, "test1").expect("Create test input file");
+    writeln!(test_input_file2, "test2").expect("Create test input file");
 
-    let file_name = "./output.txt";
-    let guard = StdoutOverride::override_file(file_name).expect("File override failed");
-    handle_config(&test_input_path_string); 
-    let contents = fs::read_to_string(file_name).expect("File read failed");
-    
-    assert!(contents.contains(expected_output));
-    
-    fs::remove_file("./output.txt").expect("File delete");    
-    drop(guard);
-    drop(test_input_file);
+    let out_dir = "./out".to_owned();
+    fs::create_dir_all(&out_dir).expect("Create test output directory");
+
+    let input_dir = fs::read_dir(&temp_dir).expect("Read input directory");
+    convert_files_in_directory(input_dir, &out_dir, "en");
+
+    let expected_output1 = HTML_TEMPLATE
+      .replace("{{title}}", "html_template_test1")
+      .replace("{{lang}}", "en")
+      + "\t<p>\n\t\ttest1\n\n\t</p>\n</body>\n</html>\n";
+    let expected_output2 = HTML_TEMPLATE
+      .replace("{{title}}", "html_template_test2")
+      .replace("{{lang}}", "en")
+      + "\t<p>\n\t\ttest2\n\n\t</p>\n</body>\n</html>\n";
+
+    let output_file1 = "./out/html_template_test1.html";
+    let output_file2 = "./out/html_template_test2.html";
+    let mut test_output_file1 = File::open(output_file1).unwrap();
+    let mut test_output_file2 = File::open(output_file2).unwrap();
+    let mut converted_string1 = String::new();
+    let mut converted_string2 = String::new();
+    test_output_file1
+      .read_to_string(&mut converted_string1)
+      .expect("Read test output file");
+    test_output_file2
+      .read_to_string(&mut converted_string2)
+      .expect("Read test output file");
+
+    assert_eq!(converted_string1, expected_output1);
+    assert_eq!(converted_string2, expected_output2);
+
+    fs::remove_dir_all(&out_dir).expect("Remove output directory");
+    drop(test_input_file1);
+    drop(test_input_file2);
     temp_dir.close().expect("Delete test directory");
   }
 }
